d49bf483a65e250eb68a61974675abef9b0a7288
eliott <eliott.jaquier@gmail.com>
2025-02-07 09:12:42 +0100

    Create address model

---
+++ db/migrate/20250131081557_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[8.0]
+  def change
+    create_table :addresses do |t|
+      t.integer :id
+      t.integer :zip
+      t.string :town
+      t.string :street
+      t.string :number
+      t.references :people, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
