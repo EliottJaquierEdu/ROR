2533965b2abf36fdd4871357fc10e50367eff7d0
eliott <eliott.jaquier@gmail.com>
2025-02-21 16:43:02 +0100

    Add unique index to subjects table on name column

--- db/migrate/20250207075939_create_subjects.rb
+++ db/migrate/20250207075939_create_subjects.rb
@@ -1,9 +1,11 @@
 class CreateSubjects < ActiveRecord::Migration[8.0]
   def change
     create_table :subjects do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :subjects, :name, unique: true
   end
 end
