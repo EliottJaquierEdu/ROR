d4fede04ee503d31ae64af65c240587db8be1b4f
eliott <eliott.jaquier@gmail.com>
2025-02-21 16:27:51 +0100

    Remove status reference from people migration as it is no longer required in the people table.

--- db/migrate/20250131082138_create_people.rb
+++ db/migrate/20250131082138_create_people.rb
@@ -6,7 +6,6 @@ class CreatePeople < ActiveRecord::Migration[8.0]
       t.string :firstname
       t.string :email
       t.string :phone_number
-      t.references :status, null: false, foreign_key: true
       t.references :address, null: false, foreign_key: true
       t.string :type
 
