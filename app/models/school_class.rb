13f21ec40e9c43b018f12c6da9c63c9a5eb0b96e
eliott <eliott.jaquier@gmail.com>
2025-02-21 23:55:59 +0100

    Fix STI fk problems

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,7 +1,7 @@
 class SchoolClass < ApplicationRecord
   belongs_to :room
   belongs_to :master, class_name: "Person", foreign_key: "master"
-  has_and_belongs_to_many :students, class_name: "Student", join_table: "school_classes_people"
+  has_and_belongs_to_many :students, class_name: "Person", join_table: "school_classes_people"
 
   has_many :courses
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,7 @@ class Student < Person
   belongs_to :student_status, optional: true
   validates :student_status, presence: true, if: -> { self.type == "Student" }
 
-  has_many :grades, foreign_key: "student_id", dependent: :destroy
+  has_many :grades,
+           foreign_key: "person_id",  # Changed from student_id to person_id
+           dependent: :destroy
 end
