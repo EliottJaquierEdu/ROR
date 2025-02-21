f0f4805aa92f5750cb776d762aac3a8f9f09a6a2
eliott <eliott.jaquier@gmail.com>
2025-02-21 17:56:36 +0100

    Remove association with status in Person model cause STI

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,6 @@
 class Person < ApplicationRecord
   self.inheritance_column = :type # Ensures Rails treats "type" as STI
 
-  belongs_to :status
   belongs_to :address
 
   has_and_belongs_to_many :school_classes
