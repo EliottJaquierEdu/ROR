460bff8c0c0436ec15ee5ccc7d2d8a8e300df58d
eliott <eliott.jaquier@gmail.com>
2025-02-22 00:02:45 +0100

    Rename join table and update associations in Person model.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,13 @@
 class Person < ApplicationRecord
-  has_one :address
+  has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address, allow_destroy: true
 
   has_and_belongs_to_many :school_classes
 
   belongs_to :student_status, optional: true
   validates :student_status, presence: true, if: :student?
+
+  has_many :grades, foreign_key: "person_id", dependent: :destroy, class_name: "Grade"
 
   def full_name
     "#{firstname} #{lastname}"
