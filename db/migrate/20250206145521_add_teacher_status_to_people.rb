485b5c244fad2ee2137e54c0d89b6d937cc1df5b
eliott <eliott.jaquier@gmail.com>
2025-02-21 16:27:21 +0100

    Set proper foreign key table for teacher_status reference cause plural was not automatically found in rails

--- db/migrate/20250206145521_add_teacher_status_to_people.rb
+++ db/migrate/20250206145521_add_teacher_status_to_people.rb
@@ -1,5 +1,5 @@
 class AddTeacherStatusToPeople < ActiveRecord::Migration[8.0]
   def change
-    add_reference :people, :teacher_status, null: false, foreign_key: true
+    add_reference :people, :teacher_status, null: false, foreign_key: { to_table: :teachers_statuses }
   end
 end
