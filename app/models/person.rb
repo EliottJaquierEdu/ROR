3858fd127eb01bdd3789ea1a6b9c1ebffbd88adf
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-02-28 09:20:55 +0100

    Update join table name for associations in Person and SchoolClass models

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ApplicationRecord
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address, allow_destroy: true
 
-  has_and_belongs_to_many :school_classes
+  has_and_belongs_to_many :school_classes, join_table: "people_school_classes"
 
   belongs_to :student_status, optional: true
   validates :student_status, presence: true, if: :student?
--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,7 +1,7 @@
 class SchoolClass < ApplicationRecord
   belongs_to :room
   belongs_to :master, class_name: "Person", foreign_key: "master"
-  has_and_belongs_to_many :students, class_name: "Person", join_table: "school_classes_people"
+  has_and_belongs_to_many :students, class_name: "Person", join_table: "people_school_classes"
 
   has_many :courses
 end
