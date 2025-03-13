1728b624b383f4147735359ed6b0279b2f03f6f1
eliott <eliott.jaquier@gmail.com>
2025-03-13 22:04:31 +0100

    Add dean resource routing under people controller to fix links to

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :people
   resources :students, controller: 'people'
   resources :teachers, controller: 'people'
+  resources :deans, controller: 'people'
   resources :addresses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
