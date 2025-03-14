bd2324a2f082b8ccfc187c50bac0d5af841b3db4
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-03-14 09:21:12 +0100

    Implement role-based visibility controls for students, teachers, school classes, subjects, and examinations; update home controller and views to reflect user permissions and enhance user experience based on roles.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
+  before_action :authenticate_person!
+  
   def index
-    @people = Person.all # List all people
-    @students = Student.all # List all students
-    @teachers = Teacher.all # List all teachers
+    @students = helpers.visible_students
+    @teachers = helpers.visible_teachers
+    @school_classes = helpers.visible_school_classes
+    @subjects = helpers.visible_subjects
+    @examinations = helpers.visible_examinations.order(expected_date: :desc)
   end
 end
