589054366d52eb93b8ea4411d67593db4e572537
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-03-14 08:49:55 +0100

    Implement address management authorization and visibility controls based on user roles; update views and controller actions accordingly.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,10 +1,14 @@
 class AddressesController < ApplicationController
+  before_action :authenticate_person!
   before_action :set_address, only: %i[ show edit update destroy ]
-  before_action :authorize_resource_management, only: %i[ new create edit update destroy ]
+  before_action :authorize_view, only: %i[ show ]
+  before_action :authorize_edit, only: %i[ edit update ]
+  before_action :authorize_create, only: %i[ new create ]
+  before_action :authorize_delete, only: %i[ destroy ]
 
   # GET /addresses or /addresses.json
   def index
-    @addresses = Address.all
+    @addresses = helpers.visible_addresses
   end
 
   # GET /addresses/1 or /addresses/1.json
@@ -14,6 +18,7 @@ class AddressesController < ApplicationController
   # GET /addresses/new
   def new
     @address = Address.new
+    @address.person_id = params[:person_id] if params[:person_id].present?
   end
 
   # GET /addresses/1/edit
@@ -61,11 +66,36 @@ class AddressesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Address.find(params.expect(:id))
+      @address = Address.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def address_params
-      params.expect(address: [ :id, :zip, :town, :street, :number, :person_id ])
+      params.require(:address).permit(:zip, :town, :street, :number, :person_id)
+    end
+    
+    # Authorization methods
+    def authorize_view
+      unless helpers.can_view_address?(@address)
+        redirect_to addresses_path, alert: "You are not authorized to view this address."
+      end
+    end
+    
+    def authorize_edit
+      unless helpers.can_edit_address?(@address)
+        redirect_to addresses_path, alert: "You are not authorized to edit this address."
+      end
+    end
+    
+    def authorize_create
+      unless helpers.can_create_address?
+        redirect_to addresses_path, alert: "You are not authorized to create addresses."
+      end
+    end
+    
+    def authorize_delete
+      unless helpers.can_delete_address?(@address)
+        redirect_to addresses_path, alert: "You are not authorized to delete this address."
+      end
     end
 end
