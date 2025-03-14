6960c4518a45cf29c2bb668cff42d05358d4f23b
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2025-03-14 08:38:04 +0100

    Add custom new action in SessionsController to handle Person to String conversion issue and improve turbo_stream response

--- app/controllers/people/sessions_controller.rb
+++ app/controllers/people/sessions_controller.rb
@@ -1,6 +1,19 @@
 class People::SessionsController < Devise::SessionsController
   respond_to :html, :turbo_stream
 
+  # Add a custom new action to fix the Person to String conversion issue
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_to do |format|
+      format.html { render :new }
+      format.json { render json: { success: false } }
+      # For turbo_stream, redirect to the HTML version instead of trying to render a template
+      format.turbo_stream { redirect_to new_person_session_path(format: :html) }
+    end
+  end
+
   # Override create to properly handle the response
   def create
     self.resource = warden.authenticate!(auth_options)
